e4ca64c8f3f133eae6eae3838a2ce5909516f251
juggler434 <wgallagher86@gmail.com>
2019-08-21 22:55:54 -0700

    implements villain interface and struct

---
+++ internal/villains/villain.go
@@ -0,0 +1,92 @@
+package villains
+
+import helpers "github.com/juggler434/marvelServer/pkg"
+
+type Villain interface {
+	IsStunned() bool
+	ApplyStun()
+	ClearStun()
+	IsTough() bool
+	ApplyTough()
+	ClearTough()
+	ApplyDamage(amount int)
+	ApplyHealing(amount int)
+	GetMaxHitPoints() int
+	GetCurrentHitPoints() int
+	GetAttack() int
+	GetScheme() int
+}
+
+type VillainStats struct {
+	maxHitPoints     int
+	currentHitPoints int
+	stunned          bool
+	tough            bool
+	attack           int
+	scheme           int
+}
+
+type VillainParams struct {
+	MaxHitPoints int
+	Attack       int
+	Scheme       int
+}
+
+func NewVillain(p *VillainParams) *VillainStats {
+	return &VillainStats{
+		maxHitPoints:     p.MaxHitPoints,
+		currentHitPoints: p.MaxHitPoints,
+		stunned:          false,
+		tough:            false,
+		attack:           p.Attack,
+		scheme:           p.Scheme,
+	}
+}
+
+func (v *VillainStats) IsStunned() bool {
+	return v.stunned
+}
+
+func (v *VillainStats) ApplyStun() {
+	v.stunned = true
+}
+
+func (v *VillainStats) ClearStun() {
+	v.stunned = false
+}
+
+func (v *VillainStats) IsTough() bool {
+	return v.tough
+}
+
+func (v *VillainStats) ApplyTough() {
+	v.tough = true
+}
+
+func (v *VillainStats) ClearTough() {
+	v.tough = false
+}
+
+func (v *VillainStats) ApplyDamage(amount int) {
+	v.currentHitPoints = helpers.Max(v.currentHitPoints-amount, 0)
+}
+
+func (v *VillainStats) ApplyHealing(amount int) {
+	v.currentHitPoints = helpers.Min(v.currentHitPoints+amount, v.maxHitPoints)
+}
+
+func (v *VillainStats) GetMaxHitPoints() int {
+	return v.maxHitPoints
+}
+
+func (v *VillainStats) GetCurrentHitPoints() int {
+	return v.currentHitPoints
+}
+
+func (v *VillainStats) GetAttack() int {
+	return v.attack
+}
+
+func (v *VillainStats) GetScheme() int {
+	return v.scheme
+}
---
+++ internal/villains/villains_test.go
@@ -0,0 +1,121 @@
+package villains_test
+
+import (
+	"testing"
+
+	"github.com/juggler434/marvelServer/internal/villains"
+)
+
+const (
+	TestAttack          = 2
+	TestScheme          = 1
+	TestHitPoints       = 14
+	TestDamage          = 5
+	TestOverkillDamage  = 100
+	TestHealing         = 3
+	TestOverkillHealing = 100
+)
+
+func createTestVillain() *villains.VillainStats {
+	p := villains.VillainParams{
+		MaxHitPoints: TestHitPoints,
+		Attack:       TestAttack,
+		Scheme:       TestScheme,
+	}
+
+	return villains.NewVillain(&p)
+}
+
+func TestVillainStats_HitPointGettersSetters(t *testing.T) {
+	v := createTestVillain()
+
+	mhp := v.GetMaxHitPoints()
+	if mhp != TestHitPoints {
+		t.Errorf("GetMaxHitPoints should return a villains max hp.  Expected: %d, got %d", TestHitPoints, mhp)
+	}
+
+	chp := v.GetCurrentHitPoints()
+	if chp != TestHitPoints {
+		t.Errorf("GetCurrentHitPoints should return the villains current hit points.  Expected %d, got %d", TestHitPoints, chp)
+	}
+
+	v.ApplyDamage(TestDamage)
+	chp = v.GetCurrentHitPoints()
+	if chp != mhp-TestDamage {
+		t.Errorf("ApplyDamage should reduce the villains current hit points by the damage amount.  Expected: %d, got: %d", mhp-TestDamage, chp)
+	}
+
+	mhp = v.GetMaxHitPoints()
+	if mhp != TestHitPoints {
+		t.Errorf("ApplyDamage should not change max hit points.  Expected: %d, got %d", TestHitPoints, mhp)
+	}
+
+	v.ApplyDamage(TestOverkillDamage)
+	chp = v.GetCurrentHitPoints()
+	if chp != 0 {
+		t.Errorf("ApplyDamage with damage over current hit points should set currenHitPoints to 0.  Expected: 0, got: %d", chp)
+	}
+
+	v.ApplyHealing(TestHealing)
+	ehp := chp + TestHealing
+	chp = v.GetCurrentHitPoints()
+	if chp != ehp {
+		t.Errorf("ApplyHealing should raise currentHitPoints by the healing amount.  Expected: %d, got: %d", ehp, chp)
+	}
+
+	v.ApplyHealing(TestOverkillHealing)
+	chp = v.GetCurrentHitPoints()
+	if chp != v.GetMaxHitPoints() {
+		t.Errorf("ApplyHealing should not raise a villains currentHealth above their maxHealth.  Expected: %d, got: %d", v.GetMaxHitPoints(), chp)
+	}
+}
+
+func TestVillainStats_GetAttack(t *testing.T) {
+	v := createTestVillain()
+	a := v.GetAttack()
+	if a != TestAttack {
+		t.Errorf("GetAttack should return a villains attack.  Expected: %d, got: %d", TestAttack, a)
+	}
+}
+
+func TestVillainStats_GetScheme(t *testing.T) {
+	v := createTestVillain()
+	s := v.GetScheme()
+	if s != TestScheme {
+		t.Errorf("GetScheme should return a villains scheme.  Expected: %d, got: %d", TestScheme, s)
+	}
+}
+
+func TestVillainStats_StunGetterSetter(t *testing.T) {
+	v := createTestVillain()
+	if v.IsStunned() {
+		t.Error("IsStunned should return if the villain is stunned.  Expected: false, got: true")
+	}
+
+	v.ApplyStun()
+	if !v.IsStunned() {
+		t.Errorf("ApplyStun should set isStunned to true. Expected: true, got: false")
+	}
+
+	v.ClearStun()
+	if v.IsStunned() {
+		t.Errorf("ClearStun should set isStunned to false.  Expected: false, got: true")
+	}
+}
+
+func TestVillainStats_ToughGetterSetter(t *testing.T) {
+	v := createTestVillain()
+	if v.IsTough() {
+		t.Errorf("IsTough should return a villains tough status. Expected: false, got: true")
+	}
+
+	v.ApplyTough()
+	if !v.IsTough() {
+		t.Errorf("ApplyTough should set tough to true.  Expected true, got: false")
+	}
+
+	v.ClearTough()
+	if v.IsTough() {
+		t.Errorf("ClearTough should set tough to false.  Expected: false, got: true")
+	}
+}
