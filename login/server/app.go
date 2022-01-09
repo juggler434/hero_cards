b784ceb61947c8c770f44c488dec25b7981d9800
juggler434 <wgallagher86@gmail.com>
2022-01-08 16:59:47 -0800

    removes exported function

--- login/server/app.go
+++ login/server/app.go
@@ -9,11 +9,3 @@ type server struct {
 	router *http.ServeMux
 	db     *sql.DB
 }
-
-// NewServer returns an instance of the loging server with a router and db
-func NewServer(router *http.ServeMux, db *sql.DB) *server {
-	return &server{
-		router: router,
-		db:     db,
-	}
-}
