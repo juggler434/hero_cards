b4a563355219717eac56713b94821fa8e18c3bc2
juggler434 <wgallagher86@gmail.com>
2022-01-10 14:05:01 -0800

    adds in comments on exported types

--- login/app/app.go
+++ login/app/app.go
@@ -5,11 +5,14 @@ import (
 	"net/http"
 )
 
+//Server hold all of the necessary components to run the login server
 type Server struct {
 	router *http.ServeMux
 	db     *sql.DB
 }
 
+//StartServer starts the server listenting on the passed in port
+// ex: login.StartServer(":8080")
 func StartServer(port string) {
 	s := &Server{
 		router: http.NewServeMux(),
